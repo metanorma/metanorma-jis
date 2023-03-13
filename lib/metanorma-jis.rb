e6a507c194fe1644ee211daf614330becafaba58
Nick Nicholas <opoudjis@gmail.com>
2023-03-14 01:19:21 +1100

    init

--- lib/metanorma-jis.rb
+++ lib/metanorma-jis.rb
@@ -2,15 +2,13 @@ require "asciidoctor" unless defined? Asciidoctor::Converter
 require_relative "metanorma/jis/converter"
 require_relative "metanorma/jis/version"
 require "isodoc/jis/html_convert"
-require "isodoc/jis/sts_convert"
 require "isodoc/jis/pdf_convert"
 require "isodoc/jis/presentation_xml_convert"
 require "isodoc/jis/metadata"
-require "isodoc/jis/xref"
 require "metanorma"
 
 if defined? Metanorma::Registry
-  require_relative "metanorma/bsi"
+  require_relative "metanorma/jis"
   Metanorma::Registry.instance.register(Metanorma::JIS::Processor)
 end
 
