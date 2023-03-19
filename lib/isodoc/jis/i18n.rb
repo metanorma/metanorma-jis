227dfce5910d99f3d1b21c866927fdb2ef3baee1
Nick Nicholas <opoudjis@gmail.com>
2023-03-20 03:12:46 +1100

    i18n restructure

--- lib/isodoc/jis/i18n.rb
+++ lib/isodoc/jis/i18n.rb
@@ -2,6 +2,7 @@ module IsoDoc
   module JIS
     class I18n < IsoDoc::Iso::I18n
       def load_yaml1(lang, script)
+        require "debug"; binding.b
         y = if lang == "ja"
               YAML.load_file(File.join(File.dirname(__FILE__), "i18n-ja.yaml"))
             else
