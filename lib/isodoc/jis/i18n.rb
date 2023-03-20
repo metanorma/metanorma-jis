1fb9d437fc405bb83bea35fa33514aae15f47f88
Nick Nicholas <opoudjis@gmail.com>
2023-03-20 20:53:29 +1100

    rspec

--- lib/isodoc/jis/i18n.rb
+++ lib/isodoc/jis/i18n.rb
@@ -2,8 +2,8 @@ module IsoDoc
   module JIS
     class I18n < IsoDoc::Iso::I18n
       def load_yaml1(lang, script)
-        y = if lang == "ja"
-              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-ja.yaml"))
+        y = if lang == "en"
+              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
             else
               YAML.load_file(File.join(File.dirname(__FILE__), "i18n-ja.yaml"))
             end
