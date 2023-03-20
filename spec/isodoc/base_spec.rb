c9f34f7638d8d4c6cdd90a75957f695222b31b0b
Nick Nicholas <opoudjis@gmail.com>
2023-03-20 20:55:26 +1100

    rspec

---
+++ spec/isodoc/base_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+require "fileutils"
+
+RSpec.describe IsoDoc::JIS do
+  it "processes isodoc as JIS: HTML output" do
+    IsoDoc::JIS::HtmlConvert.new({}).convert("test", <<~"INPUT", false)
+      <iso-standard xmlns="http://riboseinc.com/isoxml">
+        <preface>
+          <foreword>
+            <note>
+              <p id="_f06fd0d1-a203-4f3d-a515-0bdba0f8d83f">These results are based on a study carried out on three different types of kernel.</p>
+            </note>
+          </foreword>
+        </preface>
+      </iso-standard>
+    INPUT
+    html = File.read("test.html", encoding: "utf-8")
+    expect(html)
+      .to match(%r[\bpre[^{]+\{[^{]+font-family: "Courier New", monospace;]m)
+    expect(html)
+      .to match(%r[blockquote[^{]+\{[^{]+font-family: "MS Mincho", serif;]m)
+    expect(html)
+      .to match(%r[\.h2Annex[^{]+\{[^{]+font-family: "MS Gothic", serif;]m)
+  end
+end
