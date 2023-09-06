24d38d5cdffe9d3c77d6c072752d7d64b0ea2d19
Nick Nicholas <opoudjis@gmail.com>
2023-09-07 00:16:51 +1000

    https://github.com/metanorma/metanorma-standoc/issues/817

--- lib/metanorma/jis/validate.rb
+++ lib/metanorma/jis/validate.rb
@@ -1,12 +1,11 @@
 module Metanorma
   module JIS
     class Converter < ISO::Converter
-      def doctype_validate(xmldoc)
-        doctype = xmldoc&.at("//bibdata/ext/doctype")&.text
+      def doctype_validate(_xmldoc)
         %w(japanese-industrial-standard technical-report
-           technical-specification amendment).include? doctype or
+           technical-specification amendment).include? @doctype or
           @log.add("Document Attributes", nil,
-                   "#{doctype} is not a recognised document type")
+                   "#{@doctype} is not a recognised document type")
       end
 
       def script_validate(xmldoc)
