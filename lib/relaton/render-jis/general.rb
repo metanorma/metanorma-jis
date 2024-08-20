214dea5c34113772a66479f9e5f5cb773065711e
Nick Nicholas <opoudjis@gmail.com>
2024-08-21 00:00:41 +1000

    strip final period in references: https://github.com/metanorma/mn-samples-plateau/issues/148

--- lib/relaton/render-jis/general.rb
+++ lib/relaton/render-jis/general.rb
@@ -7,13 +7,20 @@ module Relaton
   module Render
     module JIS
       class General < ::Relaton::Render::Iso::General
-        #def config_loc
+        # def config_loc
         #  YAML.load_file(File.join(File.dirname(__FILE__), "config.yml"))
-        #end
+        # end
 
         def klass_initialize(_options)
           super
           @parseklass = Relaton::Render::JIS::Parse
+        end
+
+        def render1(doc)
+          r = doc.relation.select { |x| x.type == "hasRepresentation" }
+            .map { |x| @i18n.also_pub_as + render_single_bibitem(x.bibitem) }
+          out = [render_single_bibitem(doc)] + r
+          @i18n.l10n(out.join(". ").gsub(".. ", ". ").sub(/\.\s*$/, ""))
         end
       end
     end
