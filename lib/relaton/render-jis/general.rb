6bdf227730c3f139be8eb95ade82d0477a9c5687
Nick Nicholas <opoudjis@gmail.com>
2024-08-21 00:50:39 +1000

    strip final period in references; strip italics and smallcaps in refs template: https://github.com/metanorma/mn-samples-plateau/issues/148

--- lib/relaton/render-jis/general.rb
+++ lib/relaton/render-jis/general.rb
@@ -7,9 +7,9 @@ module Relaton
   module Render
     module JIS
       class General < ::Relaton::Render::Iso::General
-        # def config_loc
-        #  YAML.load_file(File.join(File.dirname(__FILE__), "config.yml"))
-        # end
+        def config_loc
+          YAML.load_file(File.join(File.dirname(__FILE__), "config.yml"))
+        end
 
         def klass_initialize(_options)
           super
@@ -21,6 +21,12 @@ module Relaton
             .map { |x| @i18n.also_pub_as + render_single_bibitem(x.bibitem) }
           out = [render_single_bibitem(doc)] + r
           @i18n.l10n(out.join(". ").gsub(".. ", ". ").sub(/\.\s*$/, ""))
+        end
+
+        def render_all(bib, type: "author-date")
+          ret = super
+          ret.each_value { |k| k[:formattedref].sub!(/[.。]\s*$/, "") }
+          ret
         end
       end
     end
